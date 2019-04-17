b5e12be414d624fa97d022cf024957463ac25665
Sergiu Voloc <38067667+SergiuVoloc@users.noreply.github.com>
2019-04-17 21:47:29 +0300

    Create UFO Game

---
+++ UFO  Game/Assets/_Completed-Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    void Update()
+    {
+        //Rotate thet transform of the game object this is attached to by 45 degrees, taking into account the time elapsed since last frame.
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+    }
+}
